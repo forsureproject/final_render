1bf56e7517ad536b2ff641430a5b8cf4193401f4
Forsure Project <forsure.project@gmail.com>
2023-03-03 08:24:53 +0000

    push for the day

--- resources/views/livewire/front/call-to-action.blade.php
+++ resources/views/livewire/front/call-to-action.blade.php
@@ -1,30 +1,32 @@
-<div  class="rounded-xl overflow-hidden bg-cover bg-center bg-no-repeat relative bg-[url({{ asset('images/architect/forsure02.jpg') }})]" >
-    <div class="">
-    {{-- <img class="absolute inset-auto w-full bg-contain bg-center" src="{{ asset('images/architect/forsure02.jpg') }}" alt=""> --}}
+<div class="bg-black/30 rounded-xl">
 
-  <div class="bg-black/25 p-8 md:p-12 lg:px-16 lg:py-24">
-    <div class="text-left sm:text-left">
-      <h2 class="text-2xl font-bold text-white sm:text-3xl md:text-5xl">
-        Our Process
-      </h2>
+    {{-- <div class="bg-black/50  absolute inset-0  z-10 "></div> --}}
+    <div class="p-8 md:p-12 md:px-16 md:py-24 rounded-xl ">
+        <div class="text-left sm:text-left ">
+            <h2 class="text-2xl font-bold text-white sm:text-3xl md:text-5xl">
+                Our Process
+            </h2>
 
-      <p
-        class=" max-w-4xl text-white/90 md:mt-6 md:block md:text-lg md:leading-relaxed"
-      >
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore
-        officia corporis quasi doloribus iure architecto quae voluptatum beatae
-        excepturi dolores.
-      </p>
+            <div class="md:flex justify-between items-start w-full">
+                <div class="w-full md:w-3/4  text-white/90 md:mt-6 md:block md:text-lg md:leading-relaxed">
+                    <p>
+                        Our process from start to completion is transparent to the client and all stakeholders. We
+                        establish
+                        a budget, draft the requirements, find a suitable land, hire the right contractors, oversee
+                        construction, maintain communication, and perform a final walkthrough till completion.
+                    </p>
+                    <div class="mt-4 sm:mt-8">
+                        <a href="{{ url('process') }}"
+                            class=" cursor-pointer inline-block rounded-full bg-lime-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-lime-700  focus:ring focus:ring-lime-400">
+                            Our Seamless Process
+                        </a>
+                    </div>
+                </div>
+                <div class="w-full md:w-1/3 relative">
+                    <img class="w-3/4 md:w-full md:absolute -top-5" src="{{ asset('images/process.png') }}" alt="">
+                </div>
+            </div>
 
-      <div class="mt-4 sm:mt-8">
-        <a
-          href="{{ url('process') }}"
-          class=" cursor-pointer inline-block rounded-full bg-lime-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-lime-700  focus:ring focus:ring-yellow-400"
-        >
-          Get Yours Today
-        </a>
-      </div>
+        </div>
     </div>
-  </div>
 </div>
-
--- resources/views/process.blade.php
+++ resources/views/process.blade.php
@@ -2,23 +2,23 @@
 
     <div class=" p-5">
 
-        <div class="mx-auto max-w-[50rem] md:max-w-none space-y-4">
+        <div class="mx-autow-full  space-y-4">
 
-            <div class="space-y-4 my-5">
-                <h1 class="text-base font-semibold leading-7 text-lime-500 ">Component playground</h1>
-                <p class="mt-3 max-w-5xl text-5xl font-extrabold tracking-tight text-white text-5xl">
-                    Get a better idea of how our components work by exploring our live playground.
+            <div class="space-y-4 my-16">
+                <h1 class="text-base font-semibold leading-7 text-lime-500 mb-5 ">The Forsure Process</h1>
+                <p class="mt-3 max-w-5xl font-extrabold tracking-tight text-white text-5xl">
+                    Our Seamless process: Plan, Prepare, Frame, Install, Finish, Inspect and Certify.
                 </p>
             </div>
-            <div class="my-5">
+            <div class="">
 
                 <!-- Icon Blocks -->
                 <div class="mx-auto py-10">
                     <div class="grid grid-cols-2 md:grid-cols-3 items-center gap-3 xl:gap-6">
                         <!-- Card -->
-                        <div class="group flex gap-y-6 w-full h-full hover:bg-gray-100 rounded-lg p-5 transition-all dark:hover:bg-white/[.055]"
+                        <div class="dark:hover:bg-white/[.055] flex group h-full  rounded-lg transition-all w-full"
                             href="#">
-                            <svg class="flex-shrink-0 w-8 h-8 text-gray-800 mt-0.5 mr-6 dark:text-gray-200"
+                            <svg class="flex-shrink-0 w-8 h-8 text-white mt-0.5 mr-6 dark:text-gray-200"
                                 xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                 viewBox="0 0 16 16">
                                 <path
@@ -29,13 +29,168 @@
 
                             <div>
                                 <div>
-                                    <div class="block font-bold text-gray-800 dark:text-white">Build your House</div>
-                                    <p class="text-gray-600 dark:text-gray-400">
-                                        The simplest way to keep your portfolio always up-to-date.
+                                    <div class="block font-bold text-white dark:text-white font- bold">Planning and Design</div>
+                                    <p class="text-gray-300 dark:text-gray-400">
+                                        Establishing requirements, designing floor plan, and obtaining permits.
                                     </p>
                                 </div>
 
-                                <p class="mt-3 inline-flex items-center gap-x-2 text-sm font-semibold text-gray-800 dark:text-gray-200">
+                                <p class="mt-3 inline-flex items-center gap-x-2 text-sm font-semibold text-white dark:text-gray-200 hidden">
+                                    Learn more
+                                    <svg class="w-2.5 h-2.5 transition ease-in-out group-hover:translate-x-1"
+                                        width="16" height="16" viewBox="0 0 16 16" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" clip-rule="evenodd"
+                                            d="M0.975821 6.92249C0.43689 6.92249 -3.50468e-07 7.34222 -3.27835e-07 7.85999C-3.05203e-07 8.37775 0.43689 8.79749 0.975821 8.79749L12.7694 8.79748L7.60447 13.7596C7.22339 14.1257 7.22339 14.7193 7.60447 15.0854C7.98555 15.4515 8.60341 15.4515 8.98449 15.0854L15.6427 8.68862C16.1191 8.23098 16.1191 7.48899 15.6427 7.03134L8.98449 0.634573C8.60341 0.268455 7.98555 0.268456 7.60447 0.634573C7.22339 1.00069 7.22339 1.59428 7.60447 1.9604L12.7694 6.92248L0.975821 6.92249Z"
+                                            fill="currentColor" />
+                                    </svg>
+                                </p>
+                            </div>
+                        </div>
+                        <!-- End Card -->
+                        <!-- Card -->
+                        <div class="dark:hover:bg-white/[.055] flex group h-full  rounded-lg transition-all w-full"
+                            href="#">
+                            <svg class="flex-shrink-0 w-8 h-8 text-white mt-0.5 mr-6 dark:text-gray-200"
+                                xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                viewBox="0 0 16 16">
+                                <path
+                                    d="M8 5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm4 3a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zM5.5 7a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm.5 6a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
+                                <path
+                                    d="M16 8c0 3.15-1.866 2.585-3.567 2.07C11.42 9.763 10.465 9.473 10 10c-.603.683-.475 1.819-.351 2.92C9.826 14.495 9.996 16 8 16a8 8 0 1 1 8-8zm-8 7c.611 0 .654-.171.655-.176.078-.146.124-.464.07-1.119-.014-.168-.037-.37-.061-.591-.052-.464-.112-1.005-.118-1.462-.01-.707.083-1.61.704-2.314.369-.417.845-.578 1.272-.618.404-.038.812.026 1.16.104.343.077.702.186 1.025.284l.028.008c.346.105.658.199.953.266.653.148.904.083.991.024C14.717 9.38 15 9.161 15 8a7 7 0 1 0-7 7z" />
+                            </svg>
+
+                            <div>
+                                <div>
+                                    <div class="block font-bold text-white dark:text-white font- bold">Site Preparationn</div>
+                                    <p class="text-gray-300 dark:text-gray-400">
+                                        Clearing land, digging foundation, and installing utilities.
+                                    </p>
+                                </div>
+
+                            </div>
+                        </div>
+                        <!-- End Card -->
+                        <!-- Card -->
+                        <div class="dark:hover:bg-white/[.055] flex group h-full  rounded-lg transition-all w-full"
+                            href="#">
+                            <svg class="flex-shrink-0 w-8 h-8 text-white mt-0.5 mr-6 dark:text-gray-200"
+                                xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                viewBox="0 0 16 16">
+                                <path
+                                    d="M8 5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm4 3a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zM5.5 7a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm.5 6a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
+                                <path
+                                    d="M16 8c0 3.15-1.866 2.585-3.567 2.07C11.42 9.763 10.465 9.473 10 10c-.603.683-.475 1.819-.351 2.92C9.826 14.495 9.996 16 8 16a8 8 0 1 1 8-8zm-8 7c.611 0 .654-.171.655-.176.078-.146.124-.464.07-1.119-.014-.168-.037-.37-.061-.591-.052-.464-.112-1.005-.118-1.462-.01-.707.083-1.61.704-2.314.369-.417.845-.578 1.272-.618.404-.038.812.026 1.16.104.343.077.702.186 1.025.284l.028.008c.346.105.658.199.953.266.653.148.904.083.991.024C14.717 9.38 15 9.161 15 8a7 7 0 1 0-7 7z" />
+                            </svg>
+
+                            <div>
+                                <div>
+                                    <div class="block font-bold text-white dark:text-white font- bold">Framing</div>
+                                    <p class="text-gray-300 dark:text-gray-400">
+                                        Constructing the frame, roof, and walls of the house.
+                                    </p>
+                                </div>
+
+                                <p class="mt-3 inline-flex items-center gap-x-2 text-sm font-semibold text-white dark:text-gray-200">
+                                    Learn more
+                                    <svg class="w-2.5 h-2.5 transition ease-in-out group-hover:translate-x-1"
+                                        width="16" height="16" viewBox="0 0 16 16" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" clip-rule="evenodd"
+                                            d="M0.975821 6.92249C0.43689 6.92249 -3.50468e-07 7.34222 -3.27835e-07 7.85999C-3.05203e-07 8.37775 0.43689 8.79749 0.975821 8.79749L12.7694 8.79748L7.60447 13.7596C7.22339 14.1257 7.22339 14.7193 7.60447 15.0854C7.98555 15.4515 8.60341 15.4515 8.98449 15.0854L15.6427 8.68862C16.1191 8.23098 16.1191 7.48899 15.6427 7.03134L8.98449 0.634573C8.60341 0.268455 7.98555 0.268456 7.60447 0.634573C7.22339 1.00069 7.22339 1.59428 7.60447 1.9604L12.7694 6.92248L0.975821 6.92249Z"
+                                            fill="currentColor" />
+                                    </svg>
+                                </p>
+                            </div>
+                        </div>
+                        <!-- End Card -->
+                        <!-- Card -->
+                        <div class="dark:hover:bg-white/[.055] flex group h-full  rounded-lg transition-all w-full"
+                            href="#">
+                            <svg class="flex-shrink-0 w-8 h-8 text-white mt-0.5 mr-6 dark:text-gray-200"
+                                xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                viewBox="0 0 16 16">
+                                <path
+                                    d="M8 5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm4 3a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zM5.5 7a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm.5 6a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
+                                <path
+                                    d="M16 8c0 3.15-1.866 2.585-3.567 2.07C11.42 9.763 10.465 9.473 10 10c-.603.683-.475 1.819-.351 2.92C9.826 14.495 9.996 16 8 16a8 8 0 1 1 8-8zm-8 7c.611 0 .654-.171.655-.176.078-.146.124-.464.07-1.119-.014-.168-.037-.37-.061-.591-.052-.464-.112-1.005-.118-1.462-.01-.707.083-1.61.704-2.314.369-.417.845-.578 1.272-.618.404-.038.812.026 1.16.104.343.077.702.186 1.025.284l.028.008c.346.105.658.199.953.266.653.148.904.083.991.024C14.717 9.38 15 9.161 15 8a7 7 0 1 0-7 7z" />
+                            </svg>
+
+                            <div>
+                                <div>
+                                    <div class="block font-bold text-white dark:text-white font- bold">Plumbing, Electrical, and HVAC</div>
+                                    <p class="text-gray-300 dark:text-gray-400">
+                                        Installing the plumbing, electrical, and HVAC systems.
+                                    </p>
+                                </div>
+
+                                <p class="mt-3 inline-flex items-center gap-x-2 text-sm font-semibold text-white dark:text-gray-200 hidden">
+                                    Learn more
+                                    <svg class="w-2.5 h-2.5 transition ease-in-out group-hover:translate-x-1"
+                                        width="16" height="16" viewBox="0 0 16 16" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" clip-rule="evenodd"
+                                            d="M0.975821 6.92249C0.43689 6.92249 -3.50468e-07 7.34222 -3.27835e-07 7.85999C-3.05203e-07 8.37775 0.43689 8.79749 0.975821 8.79749L12.7694 8.79748L7.60447 13.7596C7.22339 14.1257 7.22339 14.7193 7.60447 15.0854C7.98555 15.4515 8.60341 15.4515 8.98449 15.0854L15.6427 8.68862C16.1191 8.23098 16.1191 7.48899 15.6427 7.03134L8.98449 0.634573C8.60341 0.268455 7.98555 0.268456 7.60447 0.634573C7.22339 1.00069 7.22339 1.59428 7.60447 1.9604L12.7694 6.92248L0.975821 6.92249Z"
+                                            fill="currentColor" />
+                                    </svg>
+                                </p>
+                            </div>
+                        </div>
+                        <!-- End Card -->
+                        <!-- Card -->
+                        <div class="dark:hover:bg-white/[.055] flex group h-full  rounded-lg transition-all w-full"
+                            href="#">
+                            <svg class="flex-shrink-0 w-8 h-8 text-white mt-0.5 mr-6 dark:text-gray-200"
+                                xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                viewBox="0 0 16 16">
+                                <path
+                                    d="M8 5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm4 3a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zM5.5 7a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm.5 6a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
+                                <path
+                                    d="M16 8c0 3.15-1.866 2.585-3.567 2.07C11.42 9.763 10.465 9.473 10 10c-.603.683-.475 1.819-.351 2.92C9.826 14.495 9.996 16 8 16a8 8 0 1 1 8-8zm-8 7c.611 0 .654-.171.655-.176.078-.146.124-.464.07-1.119-.014-.168-.037-.37-.061-.591-.052-.464-.112-1.005-.118-1.462-.01-.707.083-1.61.704-2.314.369-.417.845-.578 1.272-.618.404-.038.812.026 1.16.104.343.077.702.186 1.025.284l.028.008c.346.105.658.199.953.266.653.148.904.083.991.024C14.717 9.38 15 9.161 15 8a7 7 0 1 0-7 7z" />
+                            </svg>
+
+                            <div>
+                                <div>
+                                    <div class="block font-bold text-white dark:text-white font- bold">Interior and Exterior Finishes</div>
+                                    <p class="text-gray-300 dark:text-gray-400">
+                                        Adding insulation, drywall, paint, flooring, cabinets, and fixtures to the interior, and installing siding, roofing, and landscaping to the exterior.
+                                    </p>
+                                </div>
+
+                                <p class="mt-3 inline-flex items-center gap-x-2 text-sm font-semibold text-white dark:text-gray-200">
+                                    Learn more
+                                    <svg class="w-2.5 h-2.5 transition ease-in-out group-hover:translate-x-1"
+                                        width="16" height="16" viewBox="0 0 16 16" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path fill-rule="evenodd" clip-rule="evenodd"
+                                            d="M0.975821 6.92249C0.43689 6.92249 -3.50468e-07 7.34222 -3.27835e-07 7.85999C-3.05203e-07 8.37775 0.43689 8.79749 0.975821 8.79749L12.7694 8.79748L7.60447 13.7596C7.22339 14.1257 7.22339 14.7193 7.60447 15.0854C7.98555 15.4515 8.60341 15.4515 8.98449 15.0854L15.6427 8.68862C16.1191 8.23098 16.1191 7.48899 15.6427 7.03134L8.98449 0.634573C8.60341 0.268455 7.98555 0.268456 7.60447 0.634573C7.22339 1.00069 7.22339 1.59428 7.60447 1.9604L12.7694 6.92248L0.975821 6.92249Z"
+                                            fill="currentColor" />
+                                    </svg>
+                                </p>
+                            </div>
+                        </div>
+                        <!-- End Card -->
+                        <!-- Card -->
+                        <div class="dark:hover:bg-white/[.055] flex group h-full  rounded-lg transition-all w-full"
+                            href="#">
+                            <svg class="flex-shrink-0 w-8 h-8 text-white mt-0.5 mr-6 dark:text-gray-200"
+                                xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                viewBox="0 0 16 16">
+                                <path
+                                    d="M8 5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zm4 3a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zM5.5 7a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm.5 6a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z" />
+                                <path
+                                    d="M16 8c0 3.15-1.866 2.585-3.567 2.07C11.42 9.763 10.465 9.473 10 10c-.603.683-.475 1.819-.351 2.92C9.826 14.495 9.996 16 8 16a8 8 0 1 1 8-8zm-8 7c.611 0 .654-.171.655-.176.078-.146.124-.464.07-1.119-.014-.168-.037-.37-.061-.591-.052-.464-.112-1.005-.118-1.462-.01-.707.083-1.61.704-2.314.369-.417.845-.578 1.272-.618.404-.038.812.026 1.16.104.343.077.702.186 1.025.284l.028.008c.346.105.658.199.953.266.653.148.904.083.991.024C14.717 9.38 15 9.161 15 8a7 7 0 1 0-7 7z" />
+                            </svg>
+
+                            <div>
+                                <div>
+                                    <div class="block font-bold text-white dark:text-white font- bold">Final Inspection and Certification</div>
+                                    <p class="text-gray-300 dark:text-gray-400">
+                                        Inspecting the completed house, addressing any issues, and obtaining necessary certificates of occupancy.
+                                    </p>
+                                </div>
+
+                                <p class="mt-3 inline-flex items-center gap-x-2 text-sm font-semibold text-white dark:text-gray-200 hidden">
                                     Learn more
                                     <svg class="w-2.5 h-2.5 transition ease-in-out group-hover:translate-x-1"
                                         width="16" height="16" viewBox="0 0 16 16" fill="none"
